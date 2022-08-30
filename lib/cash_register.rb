cc8d500ff04592e93290e03006a3825223d83b03
Carlos Lugo <calugo71@gmail.com>
2022-08-30 13:58:58 -0400

    commit tree 1c57b2368431593db0779c3ad3d1de9c28d2da8b

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
 
+
+class CashRegister
+
+    attr_accessor :items, :discount, :total, :last_transaction
+  
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @items = []
+    end
+  
+  end
